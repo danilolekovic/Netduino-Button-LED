a4f9c80a8dffcbc1e96b393a196c88e6170ec468
danilolekovic <danilo@lekovic.ca>
2016-01-16 14:11:34 -0800

    Code

---
+++ ButtonLED/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace ButtonLED
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var led = new OutputPort(Pins.GPIO_PIN_D13, false);
+
+            var button = new InputPort(Pins.GPIO_PIN_D2, false, Port.ResistorMode.Disabled);
+            while (true)
+            {
+                if (!button.Read())
+                {
+                    led.Write(!led.Read());
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
